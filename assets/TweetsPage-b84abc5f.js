61beb1762ebd59b326f31ea23c5733822bcba8cb
VikaSemenchuk <111804432+VikaSemenchuk@users.noreply.github.com>
2023-05-19 19:59:45 +0000

    Deploying to gh-pages from @ VikaSemenchuk/tweets-app@ba80ca04c680efc3193d2fce6f52a1ca55c54404 🚀

--- assets/TweetsPage-b84abc5f.js
+++ assets/TweetsPage-b84abc5f.js
@@ -1,4 +1,4 @@
-import{i as r,L as e,u as n,j as t}from"./index-8e50697e.js";const a=r(e)`
+import{i as r,L as e,u as n,j as t}from"./index-ec1fddaa.js";const a=r(e)`
   display: block;
 
   width: 90px;
