b566cc6a7bf3629fc979b164dd9943a6310c881e
Vika Semenchuk <vikasemenchuk@Air-Vika.fritz.box>
2023-05-20 01:36:48 +0200

    ADDED: latest react-router-dom

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -8,7 +8,7 @@ const HomePage = () => {
       Home Page
       <p>Welcome to </p>
       {/* <Link to="tweets" state={{ from: location }}>Tweets</Link> */}
-next try 7!!!
+next try 8!!!
     </div>
   )
 }
